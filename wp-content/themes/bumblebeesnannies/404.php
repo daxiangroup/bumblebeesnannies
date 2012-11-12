627b7abb30a4bd22d131619d8f5b378b21f2c9a1
Tyler Schwartz <tylers@nationalfibre.net>
2012-11-11 23:52:29 -0500

    updates to the 404 page

--- wp-content/themes/bumblebeesnannies/404.php
+++ wp-content/themes/bumblebeesnannies/404.php
@@ -5,6 +5,9 @@
 		<div id="body">
 			<div id="four-oh-four">
 				<div class="tagline">These Aren't The Bees You're Looking For...</div>
+                <div class="description">
+                    The page you're looking for doesn't exist. Or it may have at one point, but doesn't anymore. Try the navigation above, or head back to the <a href="<?php echo bloginfo('url'); ?>">hive</a>.
+                </div>
 			</div>
 		</div>
 	</div>
