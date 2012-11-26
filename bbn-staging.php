be1a025d9558caf103d3a0a80faf55b37c656bdf
Tyler Schwartz <tylers@nationalfibre.net>
2012-11-25 22:21:31 -0500

    Staging hooks

---
+++ bbn-staging.php
@@ -0,0 +1,13 @@
+<?php
+    $conn = mysql_connect($_ENV['BBN_DB_ENV_HOST'], $_ENV['BBN_DB_ENV_USER'], $_ENV['BBN_DB_ENV_PASSWORD'], $_ENV['BBN_DB_ENV_NAME'])
+        or die('Couldn\'t connect to database: '.mysql_error());
+
+    $sql = "UPDATE bumblebeesnannies.bbnoptions
+            SET option_value = 'http://bumblebees.lab.daxiangroup.com/'
+            WHERE option_name IN ('siteurl', 'home')";
+
+    mysql_query($sql)
+        or die('Couldn\'t perform query: '.$sql.' '.mysql_error());
+
+
+        define('DB_NAME', $_ENV['BBN_DB_ENV_NAME']);
